03efbdc9fc98ef01eaa34fcc0c1de2aced7121dc
Eleyine Zarour (CHUM) <eleyine@gmail.com>
2018-08-06 11:00:16 -0400

    add data share

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,13 +1,8 @@
 docker kill repertoire_container
 docker rm repertoire_container
-docker network rm repertoire-net
-
-docker network create --driver bridge repertoire-net
 
 docker build -t repertoire_image ..
 
-# docker run --network audit-net -v c:/Users/p0001073/Repertoire/repo/container/data:/data -d --name repertoire_container -p 8080:8080 repertoire_image
-docker run -d --network host --name repertoire_container -p 8080:8080 repertoire_image
+docker run -d  -v c:/Users/p0001073/Repertoire/repo/container/data:/data --env-file secret_env_smtpchum.list --name repertoire_container -p 8080:8080 repertoire_image
 
-# docker run --rm -i -t --net=host my_image telnet localhost 25
 # docker exec -it repertoire_container bash
