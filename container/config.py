71f988d1e67ebb545f20d9d61f68792095ed01ea
Eleyine Zarour <eleyine@gmail.com>
2018-08-05 20:30:33 -0400

    some more config options

--- container/config.py
+++ container/config.py
@@ -22,7 +22,7 @@ class Config(object):
     USER_SHOW_USERNAME_DOES_NOT_EXIST = True
 
     # Flask-User settings
-    USER_APP_NAME = "Radiologie CHUM"      # Shown in and email templates and page footers
+    USER_APP_NAME = "Répertoire Radiologie CHUM"      # Shown in and email templates and page footers
     USER_ENABLE_EMAIL = True      # Enable email authentication
     USER_ENABLE_USERNAME = True    # Enable username authentication
     USER_REQUIRE_RETYPE_PASSWORD = True    # False to simplify register form
@@ -38,9 +38,17 @@ class Config(object):
 
     # MAIL
     # USER_EMAIL_SENDER_NAME = USER_APP_NAME
-    USER_EMAIL_SENDER_EMAIL = "noreply@radiologiechum.ca"
+    USER_EMAIL_SENDER_EMAIL = "neurorad@rad.ca"
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    # Internationalization
+    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_TRANSLATION_DIRECTORIES = 'translations'
+    LANGUAGES = ['fr']
+
+    # FLASK_ADMIN
+    FLASK_ADMIN_SWATCH = 'cerulean'
