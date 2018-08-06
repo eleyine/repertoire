270805f8175dece95e0b8ffab1ea2479b36e8788
Eleyine Zarour <eleyine@gmail.com>
2018-08-06 11:03:09 -0400

    add sandbox script

---
+++ container/sandbox.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+import flask_user
+from app import create_app
+app = create_app()
+
+def test_user_roles():
+    with app.app_context():
+        from app.models import Role, UserRoles
+        admin_role = Role.query.filter(Role.name == 'Admin').one_or_none()
+        if admin_role:
+            print([str(i) for i in admin_role.users])
+            # help(admin_role)
+            user_roles = UserRoles.query.filter(UserRoles.role_id == admin_role.id)
+            print([str(i) for i in user_roles.all()])
+
+def reset_users():
+    with app.app_context():
+        # delete all users except admin
+        from app.models import User
+        from app import db
+        users = User.query.all()
+        for user in users:
+            if not user.is_admin:
+                print('Delete', user)
+                db.session.delete(user)
+                db.session.commit()
+
+
+if __name__ == '__main__':
+    reset_users()
