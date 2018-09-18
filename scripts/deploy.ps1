01fdc50eff58dffd23e613584cf1989d426c7ad0
Eleyine Zarour (CHUM) <eleyine@gmail.com>
2018-09-18 13:35:40 -0400

    update deploy.ps1

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,6 +3,6 @@ docker rm repertoire_container
 
 docker build -t repertoire_image ..
 
-docker run -d  -v c:/Users/p0001073/Repertoire/repo/container/data:/data --env-file secret_env_smtpchum.list --name repertoire_container -p 8080:8080 repertoire_image
+docker run -d  -v c:/Users/p0001073/Repertoire/repo/container/data:/data -e LISTEN_PORT=5001 --env-file secret_env_smtpchum.list --name repertoire_container -p 5001:5001 repertoire_image
 
 # docker exec -it repertoire_container bash
